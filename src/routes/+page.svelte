cb6a5674e2eea32b46b9d07ba14b25663afc398a
RickVellingaa <rickvellinga2408@gmail.com>
2023-09-05 21:38:44 +0200

    test

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let data;
+    console.log();
+</script>
+
+<pre>
+    {JSON.stringify(data,null,2)}
+</pre>
+
+<h1>{data.page.data.title[0].text}</h1>
+<p>{data.page.data.slices[0].primary.over[0].text}</p>
